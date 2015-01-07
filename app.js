03833d698cb9440fb90b937393d50cb636cb0c39
andela-potemuyiwa <prosper.otemuyiwa@andela.co>
2015-01-07 12:10:38 +0100

    Updated routes in app.js

--- app.js
+++ app.js
@@ -8,43 +8,41 @@ var router = express.Router();
 var app = express();
 
 
-
-
 var dances = [
 
    {
 
-   	  "name" : "Moonwalk",
+   	  "name" : "moonwalk",
    	  "creator" : "Michael Jackson",
    	  "where_created": "Atlanta,USA",
    	  "popularity" : 2
    },
    {
 
-   	  "name" : "HighLife",
+   	  "name" : "highlife",
    	  "creator" : "Osadebe",
    	  "where_created": "Enugu,Nigeria",
    	  "popularity" : 3
    },
    {
 
-   	  "name" : "Shoki",
+   	  "name" : "shoki",
    	  "creator" : "Lil Kesh",
    	  "where_created": "Lagos,Nigeria",
    	  "popularity" : 5
    },
    {
 
-   	  "name" : "Kukere",
+   	  "name" : "kukere",
    	  "creator" : "Iyanya",
    	  "where_created": "Lagos, Nigeria",
    	  "popularity" : 5
-   }
+   },
 ]
 
 
 
-
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('dev'));
 
@@ -57,6 +55,21 @@ router.get('/', function(req, res){
 
 router.get('/dances', function(req, res) {
   res.json( dances );
+});
+
+router.get('/dances/:name', function( req, res){
+
+	var dance_name = req.params.name.toLowerCase();
+    var counter = 0;
+    var value;
+	for( counter; counter <= dances.length; counter++ ){
+		if( dances[counter].name === dance_name ){
+			 res.json( dances[counter] );
+		}
+		else{
+			res.status(404).json("Not Found");
+		}
+	}
 });
 
 router.post('/dances', function(req, res, next){
